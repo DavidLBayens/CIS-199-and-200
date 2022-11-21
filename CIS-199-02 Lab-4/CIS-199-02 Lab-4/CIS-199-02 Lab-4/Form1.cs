7f69c95d5350e9f6867b3070054aa87c51c7fb49
DavidLBayens <104921435+DavidLBayens@users.noreply.github.com>
2022-11-21 18:43:10 -0500

    Add files via upload

---
+++ CIS-199-02 Lab-4/CIS-199-02 Lab-4/CIS-199-02 Lab-4/Form1.cs
@@ -0,0 +1,89 @@
+﻿// Lab 4
+// CIS-199-02
+// Due: 2/21/2021
+// By: S9223 (ID)
+
+// This is Windows form application that is designed to both accept and reject students to a 
+// university based on two credentials. The first being their highschool GPA and the second being their admissions test score.
+// This program also outputs the number of students that have been rejected and accepted by the program.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS_199_02_Lab_4
+{
+    public partial class Lab4 : Form
+    {
+        // Counter Variables
+        private int AcceptCounter = 0;
+        private int RejectCounter = 0;
+        public Lab4()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, EventArgs e)
+        {
+           
+            int Test;
+            float GPA;
+            const double GoodGPA = 3.0; // Variable for an acceptable GPA by some standards.
+            const double PerfectGPA = 4.0; // Variable for the max possible GPA in this instance.
+            const double TerribleGPA = 0; // Variable for the lowest possible GPA.
+            const int GoodTest = 80; // Variable for an acceptable test in.
+            const int PoorTest = 60; // Variable for an acceptable test based on GPA. 
+            const int PerfectTest = 100; // The Highest possible test score.
+            const int TerribleTest = 0; // The Worst possible test score.
+
+            // The variables that are inputed by the user and the output that would occur based
+            // on what is typed into the textboxes.
+            if (float.TryParse(GPAinput.Text, out GPA))
+                if (int.TryParse(ATSinput.Text, out Test))
+                    // The main code for the form that determines who would be accepted or rejected. Or alternatively if the input was invalid.
+                {
+                    if ((GPA >= GoodGPA) && (Test >= PoorTest) && (GPA <= PerfectGPA) && (Test <= PerfectTest))
+                        MessageBox.Show("Accepted");
+
+                    else if ((GPA >= TerribleGPA) && (Test >= GoodTest) && (Test <= PerfectTest) && (GPA <= PerfectGPA))
+                        MessageBox.Show("Accepted");
+
+                    else if ((GPA > PerfectGPA) || (GPA < TerribleGPA))
+                        MessageBox.Show("Invalid Credentials");
+
+                    else if ((Test > PerfectTest) || (Test < TerribleTest))
+                        MessageBox.Show("Invalid Credentials");
+
+                    else MessageBox.Show("Rejected");
+                }
+
+
+
+            if (float.TryParse(GPAinput.Text, out GPA))
+                if (int.TryParse(ATSinput.Text, out Test))
+                    // The code that is used to dictate rather or not the acceptance counter increases or the 
+                    // Rejected counter increses.
+                { 
+                    if ((GPA >= GoodGPA) && (Test >= PoorTest) && (GPA <= PerfectGPA) && (Test <= PerfectTest))
+                        ++AcceptCounter;
+
+                    else if ((GPA >= TerribleGPA) && (Test >= GoodTest) && (Test <= PerfectTest) && (GPA <= PerfectGPA))
+                        ++AcceptCounter;
+                   
+                    else if ((GPA > GoodGPA) && (Test < PoorTest) && (Test >= TerribleTest) && (GPA <= PerfectGPA))  
+                        ++RejectCounter;
+
+                    else if ((GPA >= TerribleGPA) && (GPA <= PerfectGPA) && (Test < GoodTest) && (Test >= TerribleTest))
+                        ++RejectCounter;
+                }
+            // Changes the Accept and Reject labels numeric values in accordance with the input and code.
+            Accepted.Text = $"{AcceptCounter}";
+            Rejected.Text = $"{RejectCounter}";
+            }
+    }
+}
---
+++ CIS-199-02 Lab-4/CIS-199-02 Lab-4/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace CIS_199_02_Lab_4
+{
+    partial class Lab4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GPAinput = new System.Windows.Forms.TextBox();
+            this.ATSinput = new System.Windows.Forms.TextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.TestButton = new System.Windows.Forms.Button();
+            this.Accepted = new System.Windows.Forms.Label();
+            this.HSGPA = new System.Windows.Forms.Label();
+            this.ATS = new System.Windows.Forms.Label();
+            this.Rejected = new System.Windows.Forms.Label();
+            this.AcceptedLabel = new System.Windows.Forms.Label();
+            this.RejectedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GPAinput
+            // 
+            this.GPAinput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.GPAinput.Location = new System.Drawing.Point(174, 62);
+            this.GPAinput.Name = "GPAinput";
+            this.GPAinput.Size = new System.Drawing.Size(100, 20);
+            this.GPAinput.TabIndex = 0;
+            // 
+            // ATSinput
+            // 
+            this.ATSinput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ATSinput.Location = new System.Drawing.Point(174, 117);
+            this.ATSinput.Name = "ATSinput";
+            this.ATSinput.Size = new System.Drawing.Size(100, 20);
+            this.ATSinput.TabIndex = 1;
+            // 
+            // Title
+            // 
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(12, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(293, 18);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Hypothetical University Admissions\r\n\r\n";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TestButton
+            // 
+            this.TestButton.Location = new System.Drawing.Point(101, 179);
+            this.TestButton.Name = "TestButton";
+            this.TestButton.Size = new System.Drawing.Size(120, 23);
+            this.TestButton.TabIndex = 3;
+            this.TestButton.Text = "Submit Credentials\r\n";
+            this.TestButton.UseVisualStyleBackColor = true;
+            this.TestButton.Click += new System.EventHandler(this.TestButton_Click);
+            // 
+            // Accepted
+            // 
+            this.Accepted.BackColor = System.Drawing.Color.Lime;
+            this.Accepted.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Accepted.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Accepted.Location = new System.Drawing.Point(191, 253);
+            this.Accepted.Name = "Accepted";
+            this.Accepted.Size = new System.Drawing.Size(100, 23);
+            this.Accepted.TabIndex = 4;
+            this.Accepted.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HSGPA
+            // 
+            this.HSGPA.AutoSize = true;
+            this.HSGPA.Location = new System.Drawing.Point(33, 65);
+            this.HSGPA.Name = "HSGPA";
+            this.HSGPA.Size = new System.Drawing.Size(88, 13);
+            this.HSGPA.TabIndex = 6;
+            this.HSGPA.Text = "Highschool GPA:";
+            // 
+            // ATS
+            // 
+            this.ATS.AutoSize = true;
+            this.ATS.Location = new System.Drawing.Point(33, 120);
+            this.ATS.Name = "ATS";
+            this.ATS.Size = new System.Drawing.Size(112, 13);
+            this.ATS.TabIndex = 7;
+            this.ATS.Text = "Admission Test Score:\r\n";
+            // 
+            // Rejected
+            // 
+            this.Rejected.BackColor = System.Drawing.Color.Red;
+            this.Rejected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Rejected.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rejected.Location = new System.Drawing.Point(191, 286);
+            this.Rejected.Name = "Rejected";
+            this.Rejected.Size = new System.Drawing.Size(100, 23);
+            this.Rejected.TabIndex = 8;
+            this.Rejected.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AcceptedLabel
+            // 
+            this.AcceptedLabel.AutoSize = true;
+            this.AcceptedLabel.Location = new System.Drawing.Point(52, 254);
+            this.AcceptedLabel.Name = "AcceptedLabel";
+            this.AcceptedLabel.Size = new System.Drawing.Size(96, 13);
+            this.AcceptedLabel.TabIndex = 9;
+            this.AcceptedLabel.Text = "Number Accepted:\r\n";
+            // 
+            // RejectedLabel
+            // 
+            this.RejectedLabel.AutoSize = true;
+            this.RejectedLabel.Location = new System.Drawing.Point(52, 287);
+            this.RejectedLabel.Name = "RejectedLabel";
+            this.RejectedLabel.Size = new System.Drawing.Size(93, 13);
+            this.RejectedLabel.TabIndex = 10;
+            this.RejectedLabel.Text = "Number Rejected:";
+            // 
+            // Lab4
+            // 
+            this.AcceptButton = this.TestButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 332);
+            this.Controls.Add(this.RejectedLabel);
+            this.Controls.Add(this.AcceptedLabel);
+            this.Controls.Add(this.Rejected);
+            this.Controls.Add(this.ATS);
+            this.Controls.Add(this.HSGPA);
+            this.Controls.Add(this.Accepted);
+            this.Controls.Add(this.TestButton);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.ATSinput);
+            this.Controls.Add(this.GPAinput);
+            this.Name = "Lab4";
+            this.Text = "Lab 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox GPAinput;
+        private System.Windows.Forms.TextBox ATSinput;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button TestButton;
+        private System.Windows.Forms.Label Accepted;
+        private System.Windows.Forms.Label HSGPA;
+        private System.Windows.Forms.Label ATS;
+        private System.Windows.Forms.Label Rejected;
+        private System.Windows.Forms.Label AcceptedLabel;
+        private System.Windows.Forms.Label RejectedLabel;
+    }
+}
+
